0acc858a0a4ce515d2e15c726f22ed779a567ff5
Gabrielle Glasgow <g.glasgow91@gmail.com>
2023-02-23 14:42:29 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+
+
+function distanceFromHqInBlocks(num) {
+    const block = 42
+    let distance =  Math.abs(num - block)   
+    return distance 
+}
+
+function distanceFromHqInFeet(feet) {
+    const num = distanceFromHqInBlocks(feet)
+    return num * 264
+}
+
+function distanceTravelledInFeet(start, end) {
+    //start  and end are streets
+    //find the distance between them then convert to feet
+    const block = Math.abs(start - end) * 264
+    return block
+}
+
+function calculatesFarePrice(start, destination){
+    const feet = distanceTravelledInFeet(start, destination)
+    
+    if (feet <= 400) {
+        return 0
+    } else if (feet > 400 && feet < 2000) {
+        return (feet - 400) * .02
+    } else if (feet >= 2000 && feet <= 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
